20a94801f35f1e2456f8a4ee7c564fa015cf666b
DANIEL_GRADY <anakria977@gmail.com>
2024-03-29 20:28:03 -0400

    change message

--- app/components/Landing.jsx
+++ app/components/Landing.jsx
@@ -6,7 +6,7 @@ const Landing = () => {
           <div className="welcome-details">
             <h1 className="text-left text-7xl">Welcome</h1>
             <p className="pt-10 text-lg">My name is <b>Daniel Grady</b>, I am a Full Stack Engineer based in Reno, Nevada.</p>
-            <p className="pt-5 text-lg">I am passionate about complete, jaw-dropping software solutions.</p>
+            <p className="pt-5 text-lg">I am passionate about end-to-end, intuitive software solutions.</p>
           </div>
         </div>
       </div>
